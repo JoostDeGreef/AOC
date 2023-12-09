b0d1c5fea86166310a36848da8fee8214bcaf2bd
Joost de Greef <joost@stack.nl>
2023-12-09 06:43:01 +0100

    day 9

--- AOC 2023/AOC 09/hard/hard.cpp
+++ AOC 2023/AOC 09/hard/hard.cpp
@@ -2,10 +2,64 @@
 
 using namespace std;
 
+typedef int64_t I;
+
+I ProcessSequence(const vector<I>& seq)
+{
+    I res = 0;
+    vector<I> diff;
+    I maxDiff = numeric_limits<I>::min();
+    I minDiff = numeric_limits<I>::max();
+
+    for (size_t i = 1; i < seq.size(); ++i)
+    {
+        I d = seq[i] - seq[i - 1];
+        maxDiff = max(maxDiff, d);
+        minDiff = min(minDiff, d);
+        diff.push_back(d);
+    }
+
+    if (maxDiff == minDiff)
+    {
+        return seq.back() + minDiff;
+    }
+    else
+    {
+        return seq.back() + ProcessSequence(diff);
+    }
+}
+
 int main()
 {
-    TextFile f("../../AOC 09/data/hard.txt");
+    //TextFile f("../../AOC 09/data/test.txt");
+    TextFile f("../../AOC 09/data/simple.txt");
 
+    vector<I> res;
+    vector<I> seq;
+
+    for (const auto& line : f.GetLines())
+    {
+        seq.clear();
+        if (!line.Empty())
+        {
+            const auto words = line.GetWords();
+            for (const auto& word : words)
+            {
+                seq.emplace_back(strtoll(word.c_str(), nullptr, 10));
+            }
+            seq = AOC::Reverse(seq);
+            I s = ProcessSequence(seq);
+            res.emplace_back(s);
+            cout << s << endl;
+        }
+    }
+
+    I total = 0;
+    for (I s : res)
+    {
+        total += s;
+    }
+    cout << "total = " << total << endl;
     return 0;
 }
 
